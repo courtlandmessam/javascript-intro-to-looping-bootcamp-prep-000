dc3113d2ac8ba93ec1cd69ba7412b809559ac618
Courtland Messam <messamcourtland@gmail.com>
2017-05-05 22:30:19 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+var forLoop = function(array) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1)
+      array.push("I am ${i} strange loop.")
+    else
+    array.push("I am ${i} strange loops.")
+  }
+  return array
+}
+
+var whileLoop = function(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+  return "done"
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+
+var doWhileLoop = function(array) {
+
+  do {
+    array.pop();
+  } while (array.length > 0 && maybeTrue())
+
+  return array
+
+}
